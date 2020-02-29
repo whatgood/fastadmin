3f3a65d5289e194c01218280588b41cffc03a15c
whatgood <1064692608@qq.com>
2020-02-29 17:13:27 +0800

    one

---
+++ admin/lang/zh-cn/article.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'Id'            => '序号',
+    'Title'         => '标题',
+    'Category_id'   => '分类',
+    'Content'       => '文章内容',
+    'Category.name' => '分类名称'
+];
---
+++ admin/lang/zh-cn/home.php
@@ -0,0 +1,8 @@
+<?php
+
+return [
+    'Id'      => '序号',
+    'Title'   => '标题',
+    'Content' => '内容',
+    'Status'  => '状态'
+];
